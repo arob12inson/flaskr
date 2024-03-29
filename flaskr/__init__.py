92df5c00ac405a94ce210acd7d082c6141bb3a3d
Aidan Robinson <120530116+arob26inson@users.noreply.github.com>
2024-03-29 13:22:49 -0700

    commenting __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,22 +3,26 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
+    # Create and config an app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
+    # allow custom configs, testing and otherwise
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config_from_mapping(test_config)
     
+    # ensure instance path exists
     try:
-        os.mkdir('instance')
+        os.mkdir(app.instance_path)
     except OSError:
         pass
 
+    #Create a route
     @app.route('/hello')
     def hello():
         return "Hello world!"
