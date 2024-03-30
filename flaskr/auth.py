dd2541a84adb72d4be5f38ef71042d29e9c05aff
Aidan Robinson <120530116+arob26inson@users.noreply.github.com>
2024-03-29 17:46:11 -0700

    register auth blueprint

---
+++ flaskr/auth.py
@@ -0,0 +1,12 @@
+#Blueprints: group related views & their code. App registers blueprint through factory function (i.e. create_app)
+
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth') 
